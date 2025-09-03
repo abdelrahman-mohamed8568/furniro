97f0a6442c702ea9c0bcc00333a32b7e99406a1c
Abdelrahman <124844787+abdelrahman-mohamed8568@users.noreply.github.com>
2025-09-03 20:46:47 +0300

    Fix typo in metadata title for the Furniro website

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,7 +8,7 @@ const montserrat = Montserrat({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 export const metadata = {
-  title: "Furnniro",
+  title: "Furniro",
   description: "Furniture Shopping Website",
 };
 
